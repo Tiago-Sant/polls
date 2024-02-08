3bfedc6cef1c55929658d7e6ee66bfc57afc8e79
Tiago Correa <tiagocsantana10@gmail.com>
2024-02-08 01:10:44 -0300

    feat: add redis and websocket

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 import { prisma } from "../../lib/prisma";
 import { FastifyInstance } from "fastify";
+import { redis } from "../../lib/redis";
 
 export async function getPoll(app: FastifyInstance) {
     app.get('/polls/:pollId', async (request, reply) => {
@@ -11,7 +12,7 @@ export async function getPoll(app: FastifyInstance) {
         const { pollId } = getPollParams.parse(request.params);
     
     
-        const polls = await prisma.poll.findUnique({
+        const poll = await prisma.poll.findUnique({
             where: {
                 id: pollId,
             },
@@ -24,8 +25,39 @@ export async function getPoll(app: FastifyInstance) {
                 }
             }
         })
+
+        if (!poll) {
+            return reply.status(400).send({ message: 'Poll not found' })
+        }
+
+        const result = await redis.zrange(pollId, 0, -1, 'WITHSCORES');
+
+        const votes = result.reduce((obj, line, index) => {
+            if (index % 2 === 0) {
+                return {
+                    ...obj,
+                    [line]: Number(result[index + 1])
+                }
+            }
+            return obj
+        }, {} as Record<string, number>);
         
-        return reply.send({ polls });
+        console.log(votes);
+        
+        
+        return reply.send({
+            poll: {
+                id: poll.id,
+                title: poll.title,
+                options: poll.options.map(option => {
+                    return {
+                        id: option.id,
+                        title: option.title,
+                        votes: votes[option.id] ?? 0
+                    }
+                })
+            }
+        });
     })
     
 }
